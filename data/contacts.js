ab33d38d0528665cd8b6c52bd40de5a46e6bd316
Roman Kolmakov <smtexx@hotmail.com>
2023-04-29 10:53:14 +0200

    Add contacts data

--- data/contacts.js
+++ data/contacts.js
@@ -1,4 +1,10 @@
 export const contacts = {
   phone: '+7 (903) 111-11-11',
+  address: 'Новосибирск, улица Николаева 11, офис 32',
+  map: 'https://yandex.ru/maps/-/CCUcjIHhsA',
   email: 'contact@green-gard.ru',
+  youtube: 'GreenGard54',
+  vk: 'public40719946',
+  telegram: 'green_gard_54',
+  instagram: 'green_gard_54',
 };
