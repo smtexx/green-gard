2c61dc24f3dd5cafd915c38443c64b73673c5ea5
Roman Kolmakov <smtexx@hotmail.com>
2023-04-30 11:29:04 +0200

    Create hook to hide opened menu on click

--- helpers/hooks.js
+++ helpers/hooks.js
@@ -14,3 +14,28 @@ export function useSpinner(spinnerID, dependencies) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [...dependencies]);
 }
+export function useBSNavBar(
+  menuID,
+  buttonID,
+  linkSelector,
+  useBeforeWidth
+) {
+  useEffect(() => {
+    const menu = document.getElementById(menuID);
+    const button = document.getElementById(buttonID);
+
+    function hideMenu(e) {
+      if (
+        e.target.matches(linkSelector) &&
+        window.matchMedia(`(max-width: ${useBeforeWidth}px)`).matches
+      ) {
+        button.click();
+      }
+    }
+
+    if (menu && button) {
+      menu.addEventListener('click', hideMenu);
+      return () => menu.removeEventListener('click', hideMenu);
+    }
+  }, []);
+}
